1816832aa9ac7d34070ca22fd9c6699d2c7cc717
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-26 11:53:51 +0700

    create UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\User\IUserService;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    private IUserService $userService;
+
+    /**
+     * @param IUserService $userService
+     */
+    public function __construct(IUserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $user = $this->userService->getAllUser();
+
+        return view('pages.suppliers.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
