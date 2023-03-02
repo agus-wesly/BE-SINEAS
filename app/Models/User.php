fdd96222737b799620412356184136b7ead9e51f
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-03-02 15:28:44 +0700

    create relation role and add telpon in model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'telp',
         'password',
     ];
 
@@ -42,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this->hasOne(Role::class, 'id', 'role_id')->select(['id', 'name']);
+    }
 }
