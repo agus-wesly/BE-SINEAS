be8071046f86985c72dabae5f5edd5321d10c2af
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-03-03 17:16:35 +0700

    create Repository tax

--- app/Repository/Tax/ITaxRepository.php
+++ app/Repository/Tax/ITaxRepository.php
@@ -2,7 +2,14 @@
 
 namespace App\Repository\Tax;
 
+use App\Models\Tax;
+
 interface ITaxRepository
 {
-
+    public function getAllTaxes(): object;
+    public function getTaxRate(): int|float;
+    public function getTaxAmountWithRate(int $amount, int $rate): int|float;
+    public function getTax(int $id): Tax;
+    public function createTax(array $data): void;
+    public function updateTax(array $data, Tax $tax): void;
 }
--- app/Repository/Tax/TaxRepository.php
+++ app/Repository/Tax/TaxRepository.php
@@ -2,7 +2,42 @@
 
 namespace App\Repository\Tax;
 
-class TaxRepository
-{
+use App\Models\Tax;
 
+class TaxRepository implements ITaxRepository
+{
+    private Tax $tax;
+    public function __construct(Tax $tax)
+    {
+        $this->tax = $tax;
+    }
+
+    public function getAllTaxes(): object
+    {
+        return $this->tax->all();
+    }
+    public function getTaxRate(): int|float
+    {
+        return $this->tax->firstOrFail()->price;
+    }
+
+    public function getTaxAmountWithRate($amount, $rate): int|float
+    {
+        return $amount * $rate / 100;
+    }
+
+    public function getTax(int $id): Tax
+    {
+        return $this->tax->findOrFail($id);
+    }
+
+    public function createTax(array $data): void
+    {
+        $this->tax->create($data);
+    }
+
+    public function updateTax(array $data, Tax $tax): void
+    {
+        $tax->update($data);
+    }
 }
