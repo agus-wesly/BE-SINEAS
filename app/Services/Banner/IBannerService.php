bd871d5b02640bd8c6b7b879707a2f3691f53af8
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-03-05 17:34:51 +0700

    create service banner

--- app/Services/Banner/IBannerService.php
+++ app/Services/Banner/IBannerService.php
@@ -4,5 +4,9 @@ namespace App\Services\Banner;
 
 interface IBannerService
 {
-
+    public function getAllBanner(): object;
+    public function getBannerById(int $id): object;
+    public function addBanner(array $data): void;
+    public function editBanner(array $data, $banner): void;
+    public function deleteBanner($banner): void;
 }
