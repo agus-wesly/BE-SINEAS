460e64532378be6ad7ce2115716a75ad89ce5cb0
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-03-13 00:15:43 +0700

    add 2 method

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -3,6 +3,8 @@
 namespace App\Services\User;
 
 use App\Repository\User\IUserRepository;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class UserService implements IUserService
 {
@@ -22,9 +24,40 @@ class UserService implements IUserService
         return $this->userRepository->allUser();
     }
 
+    public function getUserByEmail(string $email): object|null
+    {
+        try {
+            return $this->userRepository->getByEmail($email);
+        }catch (\Exception $e)
+        {
+            report($e);
+            throw ValidationException::withMessages([
+                'error' => 'a server error occurred'
+            ]);
+        }
+    }
+
+
     public function editUser(array $request, string $id): void
     {
         $user = $this->userRepository->getUserById($id);
         $this->userRepository->updateUser($request, $user);
     }
+
+    public function addUser(array $data)
+    {
+        try {
+            $data['password'] = Hash::make($data['password']);
+            $data['role_id'] = 2;
+            return $this->userRepository->createUser($data);
+        } catch (\Exception $e)
+        {
+            report($e);
+            throw ValidationException::withMessages([
+                'error' => 'a server error occurred'
+            ]);
+        }
+    }
+
+
 }
