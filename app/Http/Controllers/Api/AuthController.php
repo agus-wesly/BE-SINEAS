ff4721bc6f7850ad5ed489f0a186c38172b86458
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-03-20 20:37:19 +0700

    fix error

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
-use App\Http\Requests\API\RegisterRequest;
+use App\Http\Requests\Api\RegisterRequest;
 use App\Models\User;
 use App\Services\Socialite\ISocialiteService;
 use App\Services\User\IUserService;
