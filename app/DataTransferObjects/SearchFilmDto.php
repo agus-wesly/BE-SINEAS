e6bc198237ed8a4c05c65a9b9fe7bef471a49cfc
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-04-02 21:24:49 +0700

    dto search

---
+++ app/DataTransferObjects/SearchFilmDto.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use Illuminate\Http\Request;
+
+class SearchFilmDto
+{
+
+    public function __construct(
+        public readonly ?string $search = null,
+        public readonly ?bool $new = null,
+        public readonly ?string $sort = null,
+        public readonly ?int $perPage = null,
+        public readonly ?int $page = null,
+    )
+    {}
+
+    public static function fromRequest(Request $request): self
+    {
+        return new self(
+                    search: $request->search,
+                    new: $request->new,
+                    sort: $request->sort,
+                    perPage: $request->per_page,
+                    page: $request->page,
+                );
+    }
+}
