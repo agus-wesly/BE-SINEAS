07120a1e38faf70158355cfd1c09b18a116cf2a7
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-04-05 10:22:39 +0700

    resource detail film

---
+++ app/Http/Resources/FilmDetailResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FilmDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this?->id,
+            'title' => $this?->title,
+            'slug' => $this?->slug,
+            'description' => $this?->desc,
+            'url_film' => $this?->url_film,
+            'url_trailer' => $this?->url_trailer,
+            'duration' => $this?->duration,
+            'date' => $this?->date,
+            'status' => $this?->filmSelling?->status,
+            'gallery' => $this->whenLoaded('gallery'),
+            'view' => $this->whenLoaded('filmView', function () {
+                return $this->filmView->count();
+            }),
+            'information' => $this?->whenLoaded('information'),
+            'actors' => $this?->whenLoaded('actors'),
+            'filmGenre' =>$this?->whenLoaded('filmGenre'),
+            'created_at' => $this?->created_at,
+        ];
+    }
+}
