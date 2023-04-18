8aba32bc93708e38bb9e516db1028d61bd1975b7
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-04-18 23:45:36 +0700

    controller transaction

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -19,4 +19,46 @@ class TransactionController extends Controller
     {
         return $this->success('transaction success', $this->transactionService->getTransactionSuccess($request->all()));
     }
+
+    public function createOrder(Request $request)
+    {
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 10000,
+            ),
+            'customer_details' => array(
+                'first_name' => auth()->user()->name,
+                'last_name' => '',
+                'email' => auth()->user()->email,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        return $this->success('snap token', $snapToken);
+    }
+
+
+    public function callbackMidtrans(Request $request)
+    {
+       $serverKey = config('midtrans.server_key');
+       $hashed = hash('sha512', $request->order_id.$request->status_code.$request->gross_amount.$serverKey);
+         if ($hashed === $request->signature_key) {
+             if ($request->transaction_status === 'capture' || $request->transaction_status === 'settlement') {
+                $transaction = Transaction::where('no_order', $request->order_id)->first();
+                $transaction->update([
+                    'payment_status' => 'success'
+                ]);
+             }
+         }
+    }
 }
