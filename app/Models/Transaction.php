cdb92a4a73ff2b4c6ad78af5b02beddcdc8d60a6
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-04-28 00:42:08 -0700

    generet automatic code

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\Mutator\GenUid;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Transaction extends Model
 {
     use HasFactory;
-    use GenUid;
 
     protected $guarded = ['id'];
 
@@ -22,5 +22,50 @@ class Transaction extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = $model->uid();
+            }
+            $model->code = IdGenerator::generate(['table' => 'transactions','field'=>'code' , 'length' => 6, 'prefix' =>'TRX-']);
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    /**
+     * gen uid
+     * @param int $limit
+     * @return false|string
+     */
+    public function uid($limit = 16)
+    {
+        return substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, $limit);
     }
 }
