d224c3b9a5823d60366fead2e5ce1d14058bf424
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-05-16 13:07:12 +0700

    fix name table

--- database/migrations/2023_04_07_070752_create_whislists_table.php
+++ database/migrations/2023_04_07_070752_create_whislists_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\wishlist;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('whislists', function (Blueprint $table) {
+        Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
             $table->foreignUuid('user_id')->constrained('users');
             $table->foreignId('film_id')->constrained('films');
