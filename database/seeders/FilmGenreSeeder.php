b88034b6b6564e242424ee3c9c9c30a69cd0abc3
Devaanu Arkaan <arkaandevaanu@gmail.com>
2023-07-06 22:40:09 +0700

    release-production:v1.1.6

--- database/seeders/FilmGenreSeeder.php
+++ database/seeders/FilmGenreSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Genre;
 
 class FilmGenreSeeder extends Seeder
 {
@@ -12,6 +13,129 @@ class FilmGenreSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\FilmGenre::factory()->count(10)->create();
+        // \App\Models\FilmGenre::factory()->count(10)->create();
+
+        $data = [
+            [
+                "name" => 'Action',
+                "description" => "Action",
+                "slug" => "action",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Adventure',
+                "description" => "Adventure",
+                "slug" => "adventure",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Animation',
+                "description" => "Animation",
+                "slug" => "animation",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Comedy',
+                "description" => "Comedy",
+                "slug" => "comedy",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Crime',
+                "description" => "Crime",
+                "slug" => "crime",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Documentary',
+                "description" => "Documentary",
+                "slug" => "documentary",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Drama',
+                "description" => "Drama",
+                "slug" => "drama",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Family',
+                "description" => "Family",
+                "slug" => "family",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Fantasy',
+                "description" => "Fantasy",
+                "slug" => "fantasy",
+                "image" => "-"
+            ],
+            [
+                "name" => 'History',
+                "description" => "History",
+                "slug" => "history",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Horror',
+                "description" => "Horror",
+                "slug" => "horror",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Music',
+                "description" => "Music",
+                "slug" => "music",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Mystery',
+                "description" => "Mystery",
+                "slug" => "mystery",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Romance',
+                "description" => "Romance",
+                "slug" => "romance",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Science Fiction',
+                "description" => "Science Fiction",
+                "slug" => "science-fiction",
+                "image" => "-"
+            ],
+            [
+                "name" => 'TV Movie',
+                "description" => "TV Movie",
+                "slug" => "tv-movie",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Thriller',
+                "description" => "Thriller",
+                "slug" => "thriller",
+                "image" => "-"
+            ],
+            [
+                "name" => 'War',
+                "description" => "War",
+                "slug" => "war",
+                "image" => "-"
+            ],
+            [
+                "name" => 'Western',
+                "description" => "Western",
+                "slug" => "western",
+                "image" => "-"
+            ]
+            ];
+        
+
+        foreach ($data as $key => $value) {
+            Genre::create($value);
+        }
+
     }
 }
